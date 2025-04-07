3de38d7c32281667a4413eca4e2cc03345c701a7
anderson <anderdavid86@gmail.com>
2025-04-06 20:40:19 -0500

    se refactoriza clipboard

--- src/ui/ListWords.py
+++ src/ui/ListWords.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import pyttsx3
 
 class ListWords:
     def __init__(self,container):
@@ -13,15 +14,21 @@ class ListWords:
         self.listbox.grid(row=1, column=1, sticky='w', pady=10)
         self.scrollbar.config(command=self.listbox.yview)
 
+
     def insertListInFrame(self,list):
         self.listbox.delete(0, tk.END)
         for word in list:
             self.listbox.insert(tk.END, f" {word}")
         self.container.grid(row=4, column=1, sticky='w', padx=5, pady=5)
-
+        self.buttonCopyClipBoard.grid(row=6, column=1, sticky='w', padx=5, pady=5)
     def copyClipBoard(self):
             items = self.listbox.get(0,tk.END)
             textToCopy = '\n'.join(items)
             self.container.clipboard_clear()
             self.container.clipboard_append(textToCopy)
             self.container.update()
+
+    def speak_word(word):
+        engine = pyttsx3.init()
+        engine.say(word)
+        engine.runAndWait()
