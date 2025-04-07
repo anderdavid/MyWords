f795865532c0aebcff50259eb47b49dcd76c27a9
anderson <anderdavid86@gmail.com>
2025-04-06 20:27:13 -0500

    se refactorizan listas de palabras

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
 import tkinter as tk
 from src.ui.LearnedFiles import *
+from src.ui.ListWords import *
+from src.Words import *
 class Ui:
     def __init__(self):
         self.window = tk.Tk()
+        self.filename =""
         self.frameTopButtons = tk.Frame(self.window,width=300,height=50)
         self.frameTabContent = tk.Frame(self.window)
+        self.frameListContent = tk.Frame(self.window)
+        self.labelNumWords = None
+
 
     def openFile(self):
         self.frameTabContent.destroy()
         self.frameTabContent = tk.Frame(self.window)
         self.frameTabContent.grid(row =2, column =1,sticky ='w',padx=5, pady=5)
-        label = tk.Label(self.frameTabContent,text= 'open File')
-        label.grid(row=1, column=1, sticky='w',padx=5, pady=5)
+
+        filetypes = (
+            ('Text files', '*.srt'),
+        )
+        self.filename = filedialog.askopenfilename(title='Open a file .srt', filetypes=filetypes)
+        print('Selected:', self.filename)
+
+        labelFile = tk.Label(self.frameTabContent,text =self.filename)
+        labelFile.grid(row=1, column=1, padx=10, pady=10)
+
+        self.labelNumWords = tk.Label(self.frameTabContent)
+        self.labelNumWords.grid(row=2, column=1, sticky='w', padx=10, pady=10)
+
+        frameButtons = tk.Frame(self.frameTabContent, width=300, height=50)
+        frameButtons.grid(row=3, column=1, sticky='w', padx=5, pady=5)
+
+        buttonWordsOfFile = tk.Button(frameButtons, text="Words of File", command=self.wordsOfFile)
+        buttonNewWords = tk.Button(frameButtons, text="new words", command=self.newWords)
+        buttonWordsOfFile.grid(row=1, column=1, padx=5, pady=5)
+        buttonNewWords.grid(row=1, column=2, padx=5, pady=5)
+
+    def newWords(self):
+        totalWords = Words()
+        totalWords.addTotalWords()
+        mWords = Words()
+        mWords.openFile(self.filename)
+        newListWords = mWords.buildNewWords(totalWords.wordsNoRepeat, mWords.wordsNoRepeat)
+
+        self.labelNumWords.config(text=f"Number words {len(newListWords)}")
+        listWords = ListWords(container=self.frameListContent)
+        listWords.insertListInFrame(newListWords)
+
+    def wordsOfFile(self):
+        words = Words()
+        words.openFile(self.filename)
+        list = words.getWordsNoRepeatSorted()
+        self.labelNumWords.config(text=f"Number words {len(list)}")
+        listWords = ListWords(container=self.frameListContent)
+        listWords.insertListInFrame(list)
+
     def learnedWords(self):
         self.frameTabContent.destroy()
         self.frameTabContent = tk.Frame(self.window)
---
+++ src/ui/ListWords.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class ListWords:
+    def __init__(self,container):
+        self.container = container
+        self.container.grid(row=5, column=1, sticky='w', padx=5, pady=5)
+
+        self.scrollbar = tk.Scrollbar(self.container)
+        self.listbox = tk.Listbox(self.container, yscrollcommand=self.scrollbar.set, width=30, height=20, font=("Arial", 14))
+        self.buttonCopyClipBoard = tk.Button(self.container, text ="Copy to clipboard",command=self.copyClipBoard)
+
+        self.scrollbar.grid(row=1, column=1, sticky='w', padx=5, pady=5)
+        self.listbox.grid(row=1, column=1, sticky='w', pady=10)
+        self.scrollbar.config(command=self.listbox.yview)
+
+    def insertListInFrame(self,list):
+        self.listbox.delete(0, tk.END)
+        for word in list:
+            self.listbox.insert(tk.END, f" {word}")
+        self.container.grid(row=4, column=1, sticky='w', padx=5, pady=5)
+
+    def copyClipBoard(self):
+            items = self.listbox.get(0,tk.END)
+            textToCopy = '\n'.join(items)
+            self.container.clipboard_clear()
+            self.container.clipboard_append(textToCopy)
+            self.container.update()
